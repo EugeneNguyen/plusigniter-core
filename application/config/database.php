08f405eee68b6c78b4a925bc00b9db20bb8962e7
Eugene Nguyen <xuanbinh91@gmail.com>
2015-07-11 14:46:19 +0700

    remove files

--- application/config/database.php
+++ application/config/database.php
@@ -49,9 +49,9 @@ $active_group = 'default';
 $active_record = TRUE;
 
 $db['default']['hostname'] = 'localhost';
-$db['default']['username'] = '';
-$db['default']['password'] = '';
-$db['default']['database'] = '';
+$db['default']['username'] = 'root';
+$db['default']['password'] = 'beliattest';
+$db['default']['database'] = 'slap';
 $db['default']['dbdriver'] = 'mysql';
 $db['default']['dbprefix'] = '';
 $db['default']['pconnect'] = TRUE;
